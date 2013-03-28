67a62f3af8c14c62e03b5e1e07ce468f88d520d3
akhrameev <p.akhrameev@gmail.com>
2013-03-28 16:12:33 +0400

    Strang. lloks like initial commit.

---
+++ MusicStories/MSUListTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  MSUListTableViewController.h
+//  MusicStories
+//
+//  Created by Me Interactive on 25.03.13.
+//  Copyright (c) 2013 Me Interactive. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MSUListTableViewController : UITableViewController
+
+@end
